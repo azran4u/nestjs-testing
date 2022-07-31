a9518b094d1e9e4709d3385d2eec70d9584448ca
Eyal Azran <azran4u@gmail.com>
2022-07-31 09:13:24 +0300

    feat: env to string testing

--- src/utils/logAndError.ts
+++ src/utils/logAndError.ts
@@ -2,7 +2,7 @@ import { InternalServerErrorException } from '@nestjs/common';
 import { Logger } from 'winston';
 
 export function logAndThrow(error: any, logger: Logger, message: string) {
-  const errorMessage = `fetching cat facts failed. ${error?.message}`;
+  const errorMessage = `${message} ${error?.message}`;
   logger.error(errorMessage);
   throw new InternalServerErrorException(errorMessage);
 }
